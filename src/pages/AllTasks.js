613027da82ca93f5d897dce297b1620c4ddb7427
Dmytro Korobko <dmytro.korobko@gmail.com>
2024-06-19 19:01:56 -0400

    Added loading... while fetching data

--- src/pages/AllTasks.js
+++ src/pages/AllTasks.js
@@ -6,13 +6,18 @@ export function AllTasks() {
    return (
       <div className="page-content">
          <h1 className="page-caption">All Tasks</h1>
-         <div className="tasks">
-            {tasks.map((task, index) => (
-               <div key={index}>
-                  <Item task={task} />
-               </div>
-            ))}
-         </div>
+         {tasks.length > 0 ? (
+            <div className="tasks">
+               {tasks.map((task, index) => (
+                  <div key={index}>
+                     <Item task={task} />
+                  </div>
+               ))}
+            </div>
+         ) : (
+            <p>Loading...</p>
+         )}
+         
       </div>
    )
 }
